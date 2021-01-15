82b2efbf850e56fb8ce88d062530b6f5190259ab
rkdls8268 <rkdls8268@gmail.com>
2021-01-15 23:45:39 +0900

    add: useStae

---
+++ react_hooks/src/hooks/useState.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+// import "./styles.css";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(1);
+  // const [email, setEmail] = useState("");
+
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      {/* {count} */}
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+};
+export default App;
