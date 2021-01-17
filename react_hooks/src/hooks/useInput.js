42e5dff3b944d2decc0d600791ba56d31bec6c6e
rkdls8268 <rkdls8268@gmail.com>
2021-01-17 23:09:21 +0900

    add: useInput -> validator

--- react_hooks/src/hooks/useInput.js
+++ react_hooks/src/hooks/useInput.js
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
     const [value, setValue] = useState(initialValue);
     const onChange = event => {
-        console.log(event.target);
+        // console.log(event.target);
+        const {
+            target: {value}
+        } = event;
+        let willUpdate = true;
+        if (typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
     };
     return { value, onChange };
     /* return value; 와 return {value} 의 차이점
@@ -12,7 +22,11 @@ const useInput = (initialValue) => {
 }
 
 const App = () => {
-    const name = useInput('Mr.');
+    const maxLen = (item) => item.length <= 10;
+    // const maxLen = value => !value.includes("@");
+    const name = useInput('Ms.', maxLen);
+    console.log('...name: ',{...name});
+    // ...는 전개 연산자로 좌항에서 명시적으로 할당되지 않은 나머지 배열 값들을 할당
     return (
         <div>
             <h1>Hello!</h1>
